3f4906fdf890e748872942d431933c1fdf3d59b9
inieloudali <inieloud@pioix.edu.ar>
2025-10-15 10:03:28 -0300

    tabs en profesores
    no anda el tab de cerrar sesión
    
    Co-Authored-By: Tnisenbom505 <180221802+Tnisenbom505@users.noreply.github.com>

--- frontend/app/profesores/_layout.tsx
+++ frontend/app/profesores/_layout.tsx
@@ -1,29 +1,53 @@
-// app/layout.tsx
 import { Tabs } from "expo-router";
-// import { AuthProvider } from "./context/AuthContext";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from '@expo/vector-icons/Ionicons';
+// import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Layout() {
   return (
-    // <AuthProvider>
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: "tomato",
-          tabBarInactiveTintColor: "gray",
-          tabBarStyle: { height: 60, paddingBottom: 5 },
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "tomato",  
+        tabBarInactiveTintColor: "gray",  
+        tabBarStyle: { height: 60, paddingBottom: 5 },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: '',
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="home"
+              size={24}
+              color={focused ? 'tomato' : 'gray'}  
+            />
+          ),
         }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{ title: "P1" }}
-        />
-        <Tabs.Screen
-          name="about"
-          options={{ title: "P2" }}
-        />
-      </Tabs>
-    // </AuthProvider>
+      />
 
-    // Si queremos meter más screens/páginas hay que cambiar los Tab.Screen
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="settings" size={24}
+            color={ focused ? "tomato" : "gray"}/>
+          ),
+        }}
+      />
+      
+      <Tabs.Screen
+        name="sign-out"
+        options={{
+          title: "",
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome name="sign-out" size={24}
+            color={ focused ? "tomato" : "gray"} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
