8b8da1c62aa8297528c479e79bd6678cac04971a
inieloudali <inieloud@pioix.edu.ar>
2025-10-27 10:09:38 -0300

    cambio preceptores
    Co-Authored-By: Tnisenbom505 <180221802+Tnisenbom505@users.noreply.github.com>

--- frontend/app/preceptores/_layout.tsx
+++ frontend/app/preceptores/_layout.tsx
@@ -1,33 +1,46 @@
-import { Tabs } from "expo-router";
+import { Tabs } from 'expo-router';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 export default function Layout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "tomato",  
-        tabBarInactiveTintColor: "gray",  
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
         tabBarStyle: { height: 60, paddingBottom: 5 },
-      }}
-    >
+      }}>
       <Tabs.Screen
         name="index"
         options={{
           title: '',
           tabBarIcon: ({ focused }) => (
-            <FontAwesome
-              name="home"
-              size={24}
-              color={focused ? 'tomato' : 'gray'}  
-            />
+            <FontAwesome name="home" size={24} color={focused ? 'tomato' : 'gray'} />
           ),
         }}
       />
       <Tabs.Screen
-        name="about"
-        options={{ title: "P2" }}
+        name="scanner"
+        options={{
+          title: '',
+          tabBarIcon: ({ focused }) => (
+           <MaterialIcons name="qr-code-scanner" size={24} color={focused ? 'tomato' : 'gray'} />
+          ),
+        }}
       />
+
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: '',
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="settings" size={24} color={focused ? 'tomato' : 'gray'} />
+          ),
+        }}
+      />
+
     </Tabs>
   );
 }
---
+++ frontend/app/preceptores/scanner.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { View } from "react-native";
+
+export default function Scanner({}){
+    return (
+    <>
+        <View>
+            <Text>hola</Text>
+        </View>
+    </>
+)
+}
