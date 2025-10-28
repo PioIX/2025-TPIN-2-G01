9078ecc1731225ca4ced8842a166c765df1d372a
inieloudali <inieloud@pioix.edu.ar>
2025-10-28 10:05:42 -0300

    anda camera
    to do
    -limitar scaneos
    -justificar
    -socket
    
    Co-Authored-By: Tnisenbom505 <180221802+Tnisenbom505@users.noreply.github.com>

--- frontend/components/camera.tsx
+++ frontend/components/camera.tsx
@@ -1,11 +1,11 @@
-import { CameraView, CameraType, useCameraPermissions } from "expo-camera"
+import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
 import { useState } from 'react';
 import { Button, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Scanner() {
   const [facing, setFacing] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
-
+  const [scannedData, setScannedData] = useState<string | null>(null); 
   console.log('Permission status:', permission); // Debug
 
   if (!permission) {
@@ -23,21 +23,35 @@ export default function Scanner() {
     );
   }
 
+  const handleBarcodeScanned = ({ data }: { data: string }) => {
+    setScannedData(data);
+    alert(`QR Code Scanned: ${data}`); 
+  };
+
   function toggleCameraFacing() {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
 
   return (
-    <View className="flex-1 bg-green-500"> {/* Debug: fondo verde */}
+    <View className="flex-1 bg-green-500"> 
       <CameraView 
+        barcodeScannerSettings={{
+          barcodeTypes: ["qr"],
+        }}
         style={{ flex: 1 }} 
         facing={facing} 
+        onBarcodeScanned={handleBarcodeScanned} 
       />
       <View className="absolute bottom-16 flex-row w-full px-16">
         <TouchableOpacity className="flex-1 items-center bg-black/50 py-4 rounded-lg" onPress={toggleCameraFacing}>
           <Text className="text-2xl font-bold text-white">Flip Camera</Text>
         </TouchableOpacity>
       </View>
+      {scannedData && (
+        <View className="absolute top-16 w-full px-5">
+          <Text className="text-white text-center">Scanned Data: {scannedData}</Text>
+        </View>
+      )}
     </View>
   );
 }
