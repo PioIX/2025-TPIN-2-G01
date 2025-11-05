636df5cfd60fa16cdfce4e5da2463c0ca49649a5
TomasClur <toclur@pioix.edu.ar>
2025-11-05 19:00:27 -0300

    arreglo de las funciones de la pagina 2
    las funciones anda bien, solo queda implementarlas en la pagina

--- frontend/app/alumnos/about.tsx
+++ frontend/app/alumnos/about.tsx
@@ -6,19 +6,19 @@ import { useEffect, useState } from 'react';
 import useFetch from 'hooks/useFetch';
 import Button from 'components/Button';
 import { Float } from 'react-native/Libraries/Types/CodegenTypes';
-export default function AlumnosAsistencia(){
-const { data, error, loading, fetchData } = useFetch();
-const router = useRouter();
-const { token, logout } = useAuth();
-const [palabra, setPalabra] = useState<string>("")
-const [email, setEmail] = useState<string>("")
-const [fecha, setFecha] = useState<Date | null>(null)
-const [falta, setFalta] = useState<number>(0)
-const [arrayFaltas, setArrayFaltas] = useState<Array<Date|number>>([])
-/**
-   * trae la info del usuario logeado
-   * @returns {message:{datosEstudiantes}}
-   */
+export default function AlumnosAsistencia() {
+  const { data, error, loading, fetchData } = useFetch();
+  const router = useRouter();
+  const { token, logout } = useAuth();
+  const [palabra, setPalabra] = useState<string>("")
+  const [email, setEmail] = useState<string>("")
+  const [fecha, setFecha] = useState<string | null>(null)
+  const [falta, setFalta] = useState<number>(0)
+  const [arrayFaltas, setArrayFaltas] = useState<Array<string | number>>([])
+  /**
+     * trae la info del usuario logeado
+     * @returns {message:{datosEstudiantes}}
+     */
   async function fetchUser(): Promise<void> {
     const userData = await fetchData({
       url: 'http://localhost:4000/usuarioLog',
@@ -34,38 +34,44 @@ const [arrayFaltas, setArrayFaltas] = useState<Array<Date|number>>([])
     console.log("email", email)
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchUser();
-  },[])
+  }, [])
 
   useEffect(() => {
-      fetchAsistencias(email); 
-  }, [email] );
-  
+    fetchAsistencias(email);
+    console.log("array de faltas", arrayFaltas)
+  }, [email]);
+
 
   async function fetchAsistencias(correo: string): Promise<void> {
-    try{
-    const response = await fetch(
+    try {
+      const response = await fetch(
         `http://localhost:4000/traerAsistencias?correo_electronico=toclur@pioix.edu.ar&falta>=0`
       );
       const data = await response.json();
-      console.log("correo ",correo)
+      console.log("correo ", correo)
       console.log("Data crudo ", data)
       console.log("Data::: ", data.message[0]);
 
-      for (let i = 0; i<data.message.length; i++) {
-        setFecha(data.message[i].horario_de_entrada)
-        setFalta(data.message[i].falta)
-        setArrayFaltas((prevArray)=>[...prevArray, data.message[i].horario_de_entrada, data.message[i].falta])
+      for (let i = 0; i < data.message.length; i++) {
+        const fechaSoloDia = new Date(data.message[i].horario_de_entrada)
+          .toISOString()
+          .split('T')[0]; // → "2025-10-22"
+        const faltaActual = data.message[i].falta;
+        setFecha(fechaSoloDia)
+        setFalta(faltaActual)
+        setArrayFaltas(prev => [...prev, fechaSoloDia, faltaActual]);
+        //setArrayFaltas((prevArray) => [...prevArray, data.message[i].horario_de_entrada, data.message[i].falta])
       }
-      console.log(arrayFaltas)
-      }catch(error){
-        console.error("Error al traer las asistencias:", error);
-      }
+      
+    } catch (error) {
+      console.error("Error al traer las asistencias:", error);
+    }
 
   }
 
-  function testeo () {
+  function testeo() {
     setPalabra("palabra de testeo")
     console.log("email desde testeo", email)
     console.log("esto es el testeo")
@@ -78,14 +84,14 @@ const [arrayFaltas, setArrayFaltas] = useState<Array<Date|number>>([])
 
 
 
-   return (
+  return (
     <View>
       <Pressable onPress={handleLogout}>
         <Text>Cerrar sesión</Text>
-        </Pressable>
+      </Pressable>
       <Pressable onPress={testeo}>
         <Text>boton de testeo</Text>
       </Pressable>
     </View>
-   );
+  );
 }
