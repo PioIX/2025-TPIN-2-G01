47785f6d073bbf51501dfd250c44a2b1b6072cc4
TomasClur <toclur@pioix.edu.ar>
2025-11-05 10:17:21 -0300

    progreso en pagina 2 de alumno
    queda arreglar lo del hard codeo del correo y que me devuelva solo el dia en la fecha

--- frontend/app/alumnos/about.tsx
+++ frontend/app/alumnos/about.tsx
@@ -10,9 +10,11 @@ export default function AlumnosAsistencia(){
 const { data, error, loading, fetchData } = useFetch();
 const router = useRouter();
 const { token, logout } = useAuth();
+const [palabra, setPalabra] = useState<string>("")
 const [email, setEmail] = useState<string>("")
 const [fecha, setFecha] = useState<Date | null>(null)
 const [falta, setFalta] = useState<number>(0)
+const [arrayFaltas, setArrayFaltas] = useState<Array<Date|number>>([])
 /**
    * trae la info del usuario logeado
    * @returns {message:{datosEstudiantes}}
@@ -38,7 +40,7 @@ const [falta, setFalta] = useState<number>(0)
 
   useEffect(() => {
       fetchAsistencias(email); 
-  }, [] );
+  }, [email] );
   
 
   async function fetchAsistencias(correo: string): Promise<void> {
@@ -47,20 +49,26 @@ const [falta, setFalta] = useState<number>(0)
         `http://localhost:4000/traerAsistencias?correo_electronico=toclur@pioix.edu.ar&falta>=0`
       );
       const data = await response.json();
+      console.log("correo ",correo)
       console.log("Data crudo ", data)
       console.log("Data::: ", data.message[0]);
 
-      console.log("fecha antes del set", fecha)
-      console.log("falta antes del set", falta)
-      setFecha(data.fecha)
-      setFalta(data.falta)
-      console.log("fecha despues del set", fecha)
-      console.log("falta despues del set", falta)
-      
+      for (let i = 0; i<data.message.length; i++) {
+        setFecha(data.message[i].horario_de_entrada)
+        setFalta(data.message[i].falta)
+        setArrayFaltas((prevArray)=>[...prevArray, data.message[i].horario_de_entrada, data.message[i].falta])
+      }
+      console.log(arrayFaltas)
       }catch(error){
         console.error("Error al traer las asistencias:", error);
       }
 
+  }
+
+  function testeo () {
+    setPalabra("palabra de testeo")
+    console.log("email desde testeo", email)
+    console.log("esto es el testeo")
   }
 
   const handleLogout = async () => {
@@ -75,6 +83,9 @@ const [falta, setFalta] = useState<number>(0)
       <Pressable onPress={handleLogout}>
         <Text>Cerrar sesión</Text>
         </Pressable>
+      <Pressable onPress={testeo}>
+        <Text>boton de testeo</Text>
+      </Pressable>
     </View>
    );
 }
