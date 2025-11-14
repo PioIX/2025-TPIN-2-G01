53dd4bbccdf4a3b381d6903069d5cde9a2d81329
Tnisenbom505 <tnisenbom@pioix.edu.ar>
2025-11-13 23:07:55 -0300

    Update index.tsx

--- frontend/app/administradores/index.tsx
+++ frontend/app/administradores/index.tsx
@@ -1,40 +1,42 @@
-import { useRouter } from 'expo-router';
 import { useAuth } from '../context/AuthContext';
-import { Pressable, Text } from 'react-native';
-import { useEffect } from 'react';
-import NavBar from 'components/navBar';
+import { Pressable, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
 import useFetch from 'hooks/useFetch';
-export default function AdministradorHome() {
-  const menuItems = ["Inicio", "Perfil", "Ajustes"]; 
-
-  const { data, error, loading, fetchData } = useFetch();
-  const router = useRouter();
-  const { token, logout } = useAuth();
-  const handleLogout = async () => {
-    await logout();
-    router.replace('/');
-  };
+import { RolMessage } from 'types';
+export default function ProfesoresHome() {
+  const { data, error, loading, fetchData } = useFetch<RolMessage>();
+  const { token } = useAuth();
+  const [message,setMessage] = useState<string>("")
   useEffect(() => {
     if (!token) return;
+
     const fetchUser = async () => {
       const userData = await fetchData({
         url: 'http://localhost:4000/usuarioLog',
         method: 'POST',
         headers: {
           Authorization: `Bearer ${token}`,
-          Persona: 'admin',
+          Persona: 'preceptor',
         },
       });
+
+      if (userData?.message) {
+        setMessage(`Hola ${userData.message.nombre} como esta tu día `)
+        
+      } 
     };
+
     fetchUser();
   }, [token]);
+
+
+
   return (
-    <>
-    
-    <Pressable onPress={handleLogout}>
-      <Text>Cerrar sesión</Text>
-    </Pressable>
-     {/* <NavBar props={menuItems} /> */}
-    </>
+  <>
+    <View>
+      <Text>{message}</Text>
+    </View>
+  </>
   );
 }
+
